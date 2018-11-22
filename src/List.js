16dedbcef3cfcdb076dcb4736fb13533fac50de3
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2018-11-22 17:48:59 -0600

    First commit

---
+++ src/List.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Launch from '@material-ui/icons/Launch';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class CheckboxList extends React.Component {
+  state = {
+    checked: [0],
+  };
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <List>
+          {this.props.items.map((item, index) => (
+            <ListItem key={index} role={undefined} dense button onClick={this.handleToggle(index)}>
+              <Checkbox
+                checked={this.state.checked.indexOf(index) !== -1}
+                tabIndex={-1}
+                disableRipple
+              />
+              <ListItemText primary={`${item.name}`} />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Comments">
+                  <a href={`${item.url}`} target="_blank" rel="noopener noreferrer">
+                    <Launch />
+                  </a>
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+  }
+}
+
+CheckboxList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CheckboxList);
